6aecaf907f01defd9e8f2d81edbac7f8f98d9882
Ray J. Greenwell <ray@bogocorp.com>
2019-08-30 15:27:37 -0700

    Set up three.js as window.THREE.
    Also stop adding starter monsters.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import * as threejs from "three"
+window["THREE"] = threejs
+
 import {App} from "./app"
 import {RanchModel} from "./moncher"
 import {RanchMode} from "./moncher3d"
-import {MonsterDb} from "./monsterdb"
+//import {MonsterDb} from "./monsterdb"
 
 const root = document.getElementById("root")
 if (!root) throw new Error(`No root?`)
@@ -12,10 +15,10 @@ app.start()
 let ranch :RanchModel = new RanchModel()
 
 // add sample monsters at the 4 corners
-ranch.addActor(MonsterDb.getRandomEgg(), 0, 0)
-ranch.addActor(MonsterDb.getRandomEgg(), 1, 0)
-ranch.addActor(MonsterDb.getRandomEgg(), 0, 1)
-ranch.addActor(MonsterDb.getRandomEgg(), 1, 1)
+//ranch.addActor(MonsterDb.getRandomEgg(), 0, 0)
+//ranch.addActor(MonsterDb.getRandomEgg(), 1, 0)
+//ranch.addActor(MonsterDb.getRandomEgg(), 0, 1)
+//ranch.addActor(MonsterDb.getRandomEgg(), 1, 1)
 
 app.setMode(new RanchMode(app, ranch))
 
