df5e18a1a491390d0772da6e24ad578f49fdba74
Ray J. Greenwell <ray@bogocorp.com>
2019-09-05 13:33:26 -0700

    dontCallStop didn't work. Undo, and break happyReact temporarily.
    The animation might have been finished but it was still "scheduled".
    Fixing that starts to get more complex, and Andrzej has animation stuff
    coming for me anyway.

--- src/moncher3d.ts
+++ src/moncher3d.ts
@@ -618,7 +618,6 @@ export class RanchMode extends Mode
         name: "touched",
       }
       graphCfg.animReact = animation(cfg.model.happyReact, "touched", 1)
-      graphCfg.animReact.dontCallStop = true
     }
 
     if (cfg.model.wakeUp) {
