f154fe70f692880c534c4bcbfed4427f55465b38
Ray J. Greenwell <ray@bogocorp.com>
2019-09-08 18:48:41 -0700

    Nix sample monster now that buttons are touchable.

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ window["THREE"] = threejs
 import {App} from "./app"
 import {RanchModel} from "./moncher"
 import {RanchMode} from "./moncher3d"
-import {Vector3} from "three"
-import {MonsterDb} from "./monsterdb"
+//import {Vector3} from "three"
+//import {MonsterDb} from "./monsterdb"
 
 const root = document.getElementById("root")
 if (!root) throw new Error(`No root?`)
@@ -19,6 +19,6 @@ const mode :RanchMode = new RanchMode(app, ranch)
 app.setMode(mode)
 
 // add a sample monster
-ranch.addActor(MonsterDb.getRandomEgg(), new Vector3(2, -.25, 0)) // y is approximate
+//ranch.addActor(MonsterDb.getRandomEgg(), new Vector3(2, -.25, 0)) // y is approximate
 
 setInterval(() => { ranch.tick(1000) }, 1000)
