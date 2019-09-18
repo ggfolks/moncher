077cdaf4261a3c3b4f0a290e20945f4883edd10b
Ray J. Greenwell <ray@bogocorp.com>
2019-09-18 14:59:29 -0700

    Hoop-jumpery needed to load the navmesh GLB on the server.
    I still haven't actually used it but wanted to checkpoint what's
    working.

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import {Server} from "tfw/data/server"
 import {FirebaseDataStore} from "tfw/data/firebase"
 import {FirebaseAuthValidator} from "tfw/auth/firebase"
 import {ServerObject} from "./data"
+import {log} from "tfw/core/util"
+import {Pathfinding} from "./pathfinding"
 
 setTextCodec(() => new TextEncoder() as any, () => new TextDecoder() as any)
 
@@ -28,18 +30,38 @@ server.state.onValue(ss => {
 })
 
 
-var loader = require("three-gltf-loader")
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
-global["XMLHttpRequest"] = XMLHttpRequest
+// Load the navmesh GLB and put our pathfinder into global
+global["Blob"] = require("web-blob").constructor
+const Loader = require("three-gltf-loader")
+const fs = require("fs")
+fs.readFile("dist/ranch/RanchNavmesh.glb", (err :any, data :any) => {
+  if (err) {
+    log.warn("Got an error", "err", err)
+  } else {
+    if (data instanceof Buffer) {
+      log.info("It's a buffer!")
+      const loader = new Loader()
+      loader.parse(data.buffer, "./",
+        (gltf :any) => {
+          console.log("Holy crap we got it? " + gltf)
+          configureNavMesh(gltf)
+        },
+        (error :any) => {
+          console.error("I got an error " + error)
+        })
 
-// Load the ranch and just shoooove it into the global context
-new loader().load(
-  "http://localhost:3000/ranch/Ranch.glb",
-  (gltf :any) => {
-    console.log("Holy crap we got it? " + gltf)
-  },
-  (event :any) => { /* progress events */ },
-  (error :any) => {
-    console.error("I got an error " + error)
-  })
+    } else {
+      log.info("Data is", typeof data, data)
+    }
+  }
+})
 
+function configureNavMesh (gltf :any) :void {
+  const scene = gltf.scene
+  const navMesh = scene.getObjectByName("NavMesh")
+//  log.debug("I got something: " + navMesh)
+//  log.debug("It's a mesh? " + (navMesh instanceof Mesh))
+  const pather = new Pathfinding()
+  pather.setZoneData("ranch", Pathfinding.createZone(navMesh.geometry))
+  global["_ranchPathfinder"] = pather
+}
