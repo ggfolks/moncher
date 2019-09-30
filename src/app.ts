d01fafcc886f18781d074d88bd274cb0b2d30754
Michael Bayne <mdb@samskivert.com>
2019-09-30 08:45:44 -0700

    Give devs their own private Idaho.
    Now that ranches are persistent again, we don't want everyone writing to the
    same ranch record because it will be a nightmare to test things. So if you
    don't specify a ranchid (on localhost), we make up a new one and save it to
    browser local storage and then keep redirecting you to that one.
    
    But you should retrain your browsers to use your personal ranch link because we
    can't magically redirect you to the right ranch if you're using multiple
    browsers. Maybe in a few days we'll just start requiring a ranchid, so that if
    you forget, you just see a blank page instead of ending up at a randomly
    generated new ranch.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as firebase from "firebase/app"
 
 import {Disposable, Disposer} from "tfw/core/util"
 import {dim2} from "tfw/core/math"
+import {uuidv1} from "tfw/core/uuid"
 import {Clock, Loop} from "tfw/core/clock"
 import {loadImage} from "tfw/core/assets"
 import {Subject, Value} from "tfw/core/react"
@@ -37,9 +38,21 @@ function parseLocation () :[string, string, string|undefined] {
     const match = query.substring(1).match(ranchFocusR)
     if (match) return [path, match[1], match[3]]
   }
-  return [path, "5cXg8Tp5WwsuVeO7JflubY", undefined]
+  return [path, "", undefined]
 }
 const [appPath, ranchId, focusId] = parseLocation()
+
+// if we have no ranch id, redirect to one
+if (ranchId === "") {
+  if (window.location.hostname === "localhost") {
+    // on localhost, make up a unique ranchid for this developer/browser
+    let ranchId = localStorage.getItem("ranchid")
+    if (!ranchId) localStorage.setItem("ranchid", ranchId = uuidv1())
+    window.location.search = ranchId
+  } else {
+    window.location.pathname = "5cXg8Tp5WwsuVeO7JflubY"
+  }
+}
 
 export class App implements Disposable {
   private mode :Mode
