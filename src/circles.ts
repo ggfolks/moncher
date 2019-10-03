1010651e015ff22e74feb8e66623457c09cea495
Ray J. Greenwell <ray@bogocorp.com>
2019-10-03 15:22:17 -0700

    Align all points on the circle to the same maximum Y.
    That should look OK, once we have nice art for it.

--- src/circles.ts
+++ src/circles.ts
@@ -7,14 +7,18 @@ export function createChatCircle (circle :ChatCircle, setY :(into :Vector3) => v
   const points = POINTS_PER_RADIUS * circle.radius
   const center = loc2vec(circle)
   const geom = new Geometry()
+  let maxY = center.y
   for (let pp = 0, rads = 0, incr = (Math.PI * 2) / points; pp < points; pp++, rads += incr) {
     const point = center.clone()
     point.x += Math.sin(rads) * circle.radius
     point.z += Math.cos(rads) * circle.radius
-    setY(point) // best effort to find the Y of the point, otherwise keep it at the center point
+    setY(point) // best effort to find the Y of the point, otherwise keep center Y
+    maxY = Math.max(maxY, point.y)
     geom.vertices.push(point)
   }
-  const mat = new LineBasicMaterial({color: 0xEEEE22, linewidth: 10})
+  // go back through and force them all to the highest Y
+  geom.vertices.forEach(v => { v.y = maxY })
 
+  const mat = new LineBasicMaterial({color: 0xEEEE22, linewidth: 10})
   return new LineLoop(geom, mat)
 }
